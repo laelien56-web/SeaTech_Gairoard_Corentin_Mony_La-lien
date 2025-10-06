230eb80e1fcb213e4317c45d807c6d1fc575bf33
iutgeiitoulonE105 <touloniut@gmail.com>
2025-10-06 12:28:25 +0200

    fin de séance 1
    nous sommes rendus à la page 16 de la partie 1

---
+++ Embedded/Robot_Gairoard_Mony.X/PWM.h
@@ -0,0 +1,25 @@
+/* 
+ * File:   PWM.h
+ * Author: E306-PC6
+ *
+ * Created on 6 octobre 2025, 12:14
+ */
+
+#ifndef PWM_H
+#define	PWM_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+void InitPWM();
+void PWMSetSpeed(float vitesseEnPourcents);
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* PWM_H */
+
---
+++ Embedded/Robot_Gairoard_Mony.X/ToolBox.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   ToolBox.h
+ * Author: E306-PC6
+ *
+ * Created on 6 octobre 2025, 12:11
+ */
+
+#ifndef TOOLBOX_H
+#define	TOOLBOX_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* TOOLBOX_H */
+
